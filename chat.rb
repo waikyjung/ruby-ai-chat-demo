9e6bafd3424847280d6f3f76b9566a9023f78294
waikyjung <waiky.jung@gmail.com>
2024-10-15 05:21:11 +0000

    app start

--- chat.rb
+++ chat.rb
@@ -1,4 +1,28 @@
-include "openai"
+require "openai"
+require "json"
 
-pp "hi"
-client = OpenAI::Client.new(access_token: ENV.fetch("OPEN_AI_KEY"))
+class Chat
+  attr_accessor :message_list
+
+  def initialize
+    @client = OpenAI::Client.new(access_token: ENV.fetch("OPEN_AI_KEY"))
+    @message_list = [{"role" => "system", "content" => "You are a helpful assistant."}]
+  end
+
+  def start
+    until statement.downcase == "bye" do
+      @user_statement = gets.chomp
+      @message_list.push({"role" => "user", "content" => @user_statement})
+      @api_response = client.chat(
+        paramenters: {
+          model: "gpt-3.5-turbo",
+          messages: @message_list
+        }
+      
+      )
+      pp api_response
+    end
+  end
+end
+
+chat.start
